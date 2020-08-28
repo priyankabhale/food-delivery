3436e885045d4ba7c3b52b0430eb101ed86cf862
Priyanka Bhale <Priyanka.Bhale@bauer.de>
2020-08-28 18:51:37 +0530

    additional changes

---
+++ src/main/java/com/test/fooddelivery/util/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.test.fooddelivery.util;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
+  
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<Object> checkedexception(
+      Exception ex, WebRequest request) {
+
+      Map<String, Object> body = new LinkedHashMap<>();
+      body.put("timestamp", LocalDateTime.now());
+      body.put("message", "Error occurred");
+
+      return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+  
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<Object> runtimeexception(
+      RuntimeException ex, WebRequest request) {
+
+      Map<String, Object> body = new LinkedHashMap<>();
+      body.put("timestamp", LocalDateTime.now());
+      body.put("message", "Error occurred");
+
+      return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
